ccbb47ad3b7d2b16a5b85693f73f097f0abe23f2
Livia-11 <kirezilivia@gmail.com>
2025-01-30 15:01:18 +0200

    Adding student

--- src/main/java/rw/ac/rca/mis/rcamis/util/Program.java
+++ src/main/java/rw/ac/rca/mis/rcamis/util/Program.java
@@ -1,7 +1,19 @@
 package rw.ac.rca.mis.rcamis.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import rw.ac.rca.mis.rcamis.models.Student;
+
+import java.time.LocalDate;
+
 public class Program {
     public static void main(String[] args) {
-
-    }
+       SessionFactory sessionFactory = HibernateUtil.getSessionFactory() ;
+       Session session = sessionFactory.openSession();
+       Transaction transaction = session.beginTransaction();
+        Student student1 = new Student(1, "Mike", "Mugabo", "mike@gmail.com", 12, LocalDate.now());
+        session.persist(student1);
+        transaction.commit();
+     }
 }
